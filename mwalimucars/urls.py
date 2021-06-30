10f03eda6f6a8d64dcce93acd1a02cb23e9a0844
NorbertOdhiambo <odhiambonorbert194@gmail.com>
2021-06-30 16:17:34 +0300

    background jobs

--- mwalimucars/urls.py
+++ mwalimucars/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('products.urls')),
                   path('summernote/', include('django_summernote.urls')),
-                  path('django-rq/', include('django_rq.urls')),
+                  # path('django-rq/', include('django_rq.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
